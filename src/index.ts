6871fab6ec4037fc45ea08b967429f7528429ea6
lutzer <in_the_fishtank@gmx.de>
2020-04-03 17:28:13 +0200

    updated guig

--- src/index.ts
+++ src/index.ts
@@ -7,40 +7,49 @@ window.addEventListener("load", () => {
 
   app.enableSound(false)
   app.setOutput('midi')
+  
+  function randomizeApp() : number {
+    const seed = Math.floor(Math.random() * Math.pow(10,5))
+    app.randomize(seed)
+    return seed
+  }
 
   class GuiParams {
     enableOutput = false;
     bpm = 30;
     output = 'midi';
     randomize = function() {
-      this.seed = Math.floor(Math.random() * Math.pow(10,9))
-      app.randomize(this.seed)
+      this.seed = randomizeApp()
       this.bpm = app.getParams().bpm
     }
     seed = 0
   }
 
   const params = new GuiParams()
-
   const gui = new dat.GUI()
   var folder1 = gui.addFolder('Audio')
 
   folder1.add(params, 'enableOutput').listen().onChange( (val) => {
     app.enableSound(val)
   })
-  folder1.add(params, 'output', ['midi', 'piano', 'guitar']).onChange( (val) => {
+  folder1.add(params, 'output', { 
+    'Midi (Channel 1)' : 'midi', 
+    'Sampler (Piano)' : 'piano', 
+    'Sampler (Guitar)' : 'guitar' 
+  }).onChange( (val) => {
     app.setOutput(val)
     params.enableOutput = false
   })
 
   var folder2 = gui.addFolder('Settings')
   folder2.add(params, 'randomize')
-  // f2.add(params, 'seed').listen()
-  folder2.add(params, 'bpm', 10, 120).listen().onChange( (val) => {
+  folder2.add(params, 'bpm', 10, 120, 1).listen().onFinishChange( (val) => {
     app.setBpm(val)
   })
 
   folder1.open()
   folder2.open()
+
+  randomizeApp()
 
 })
